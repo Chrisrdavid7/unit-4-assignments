63ba0f728fc994583b736ab052e21617e11ff2d5
Chris David <chrisrdavid7@gmail.com>
2016-01-22 23:59:42 -0500

    commit

--- HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
+++ HWfrom1-14-16(Logic+Discrete_Math).playground/Contents.swift
@@ -5,25 +5,46 @@ import UIKit
 var str = "Hello, playground"
 
 
-/*
 
-Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
-Copy and paste your code:
+//Question 1: https://www.hackerrank.com/challenges/minimum-draws
 
-What is the big O runtime of your code?:
+//Copy and paste your code:
+func maxDraws(let n: Int) -> Int {
+    return n + 1
+}
 
-Question 2: https://www.hackerrank.com/challenges/handshake
+let numOfPairs = 3
+maxDraws(numOfPairs)
 
-Copy and paste your code:
 
-What is the big O runtime of your code?:
+//What is the big O runtime of your code?:
 
-Question 3: https://www.hackerrank.com/challenges/connecting-towns
+//Question 2: https://www.hackerrank.com/challenges/handshake
 
-Copy and paste your code:
+//Copy and paste your code:
 
-What is the big O runtime of your code?:
+func factor(var n: Int) -> Int
+{
+    var number = n - 1
+    
+    for _ in 1..<n {
+        
+        n *= number
+        number--
+        
+    }
+    
+    return n
+}
 
-*/
+//What is the big O runtime of your code?:
+
+//Question 3: https://www.hackerrank.com/challenges/connecting-towns
+
+//Copy and paste your code:
+
+//What is the big O runtime of your code?:
+
+
 
